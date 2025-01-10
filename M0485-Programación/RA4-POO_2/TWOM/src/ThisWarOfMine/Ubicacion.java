667de83b274ff50cad842690ce1ccd7b7cbb716a
CoceraCia <coceraciamiguel@gmail.com>
2025-01-10 14:10:52 +0100

    Muy pequeño avance en la clase ubicacion TWOM

--- M0485-Programación/RA4-POO_2/TWOM/src/ThisWarOfMine/Ubicacion.java
+++ M0485-Programación/RA4-POO_2/TWOM/src/ThisWarOfMine/Ubicacion.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ThisWarOfMine;
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  *
  * @author migue
  */
 public class Ubicacion {
-    public Ubicacion
+    private ArrayList<Objeto> objetos;
+    private int peligrosidad;
+    private int nivel = 1;
+    
+    public Ubicacion(){
+        Random ran = new Random();
+        this.peligrosidad = ran.nextInt(1, this.nivel + 1);
+    }
+    
+    public void incrementarNivel(){
+        this.nivel++;
+    }
+    
+    private ArrayList<Objeto> generarObjetos(){
+        Random ran = new Random();
+        ArrayList<Objeto> objetos = new ArrayList<>();
+        //generamos la cantidad de objetos que apareceran
+        int n = ran.nextInt(1,11);
+        for (int i = 0; i < n; i++ ){
+            
+        }
+        return objetos;
+    }
+    
 }
