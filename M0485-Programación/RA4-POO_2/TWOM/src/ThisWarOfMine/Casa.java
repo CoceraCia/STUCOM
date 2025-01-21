3d584c189af0050c3d5ce161f73b5d0e45e77916
CoceraCia <coceraciamiguel@gmail.com>
2025-01-21 15:29:04 +0100

    Commit en el cual estoy apunto de acabar el TWOM, hay muchas cosas en el documento las cuales no le veo sentido pero me he quedado por la funcion finDia(jugadores)-->Gastar 5 componentes a cambio de 1 medicamento

--- M0485-Programación/RA4-POO_2/TWOM/src/ThisWarOfMine/Casa.java
+++ M0485-Programación/RA4-POO_2/TWOM/src/ThisWarOfMine/Casa.java
@@ -38,4 +38,41 @@ public class Casa {
     public ArrayList<Objeto> getObjetos(){
         return this.objetos;
     }
+    
+    // Funcion para comprobar que el objeto existe
+    public boolean objExiste(String tipo) {
+        if (!this.objetos.isEmpty()) { //Si no esta vacia se ejecuta el codigo
+            for (int i = 0; i < this.objetos.size(); i++) {
+                Objeto obj = this.objetos.get(i);
+                if (obj.getTipo().equalsIgnoreCase(tipo)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    //Funcion para incrementar la cantidad de objetos para que no haya duplicidad de objetos en el Array
+    public void incrementarCantidadObj(String tipo, int cantidad) {
+        for (int i = 0; i < this.objetos.size(); i++) {
+            Objeto obj = this.objetos.get(i);
+            if (obj.getTipo().equals(tipo)) {
+                obj.setCantidad(obj.getCantidad() + cantidad);
+            }
+        }
+    }
+    
+    //Funcion para restar la cantidad de objetos para que no haya duplicidad de objetos en el Array
+    public void restarCantidadObj(String tipo, int cantidad) {
+        for (int i = 0; i < this.objetos.size(); i++) {
+            Objeto obj = this.objetos.get(i);
+            if (obj.getTipo().equals(tipo)) {
+                if (obj.getCantidad() - 1 > 0){
+                    obj.setCantidad(obj.getCantidad() - cantidad);
+                } else {
+                    objetos.remove(i);
+                }
+            }
+        }
+    }
 }
